de6103238ea12c380f693e42bd38f6cfeffae66d
Estelope <elopez14567@gmail.com>
2023-10-05 01:08:31 -0600

    add stylists model

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public List<Client> Clients { get; set; }
+  }
+}
